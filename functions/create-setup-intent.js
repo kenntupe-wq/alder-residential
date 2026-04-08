2ede98d23741599fd459ed8098aa037bb827dd38
kenntupe-wq <kenntupe@gmail.com>
2026-04-08 11:09:12 -0600

    Update create-setup-intent.js

--- functions/create-setup-intent.js
+++ functions/create-setup-intent.js
@@ -1,10 +1,11 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
 export async function onRequestPost(context) {
+  // This grabs the key from your Cloudflare Variables automatically
+  const stripe = require('stripe')(context.env.STRIPE_SECRET_KEY);
+
   try {
     const intent = await stripe.setupIntents.create({
       payment_method_types: ['card'],
-      usage: 'off_session', // This allows you to charge them later manually
+      usage: 'off_session', 
     });
 
     return new Response(JSON.stringify({ client_secret: intent.client_secret }), {
